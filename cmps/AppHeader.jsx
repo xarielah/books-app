bfec30c894b899e23c43bc84b77f42f033bf8dfd
Ariel <arielah1997@gmail.com>
2024-11-22 00:02:26 +0200

    feat: update app header

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,10 +1,16 @@
+const { Link } = ReactRouterDOM;
 
 export function AppHeader() {
 
     return (
         <header className="app-header full main-layout">
             <section className="header-container">
-                <h1>React Starter Proj</h1>
+                <h1>Miss Books</h1>
+                <div>
+                    <span style={{ marginLeft: "1em" }}><Link to="/home">Home</Link></span>
+                    <span style={{ marginLeft: "1em" }}><Link to="/about">About</Link></span>
+                    <span style={{ marginLeft: "1em" }}><Link to="/book">Books</Link></span>
+                </div>
             </section>
         </header>
     )
